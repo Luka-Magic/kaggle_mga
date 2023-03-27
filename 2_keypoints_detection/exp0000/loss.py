a61ee392851b4bd4c327c7fffa5b47affea98652
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-03-27 21:59:18 +0900

    2_exp0000

--- 2_keypoints_detection/exp0000/loss.py
+++ 2_keypoints_detection/exp0000/loss.py
@@ -12,14 +12,10 @@ class JointsMSELoss(nn.Module):
         heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
         heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
         loss = 0
-        # target_weight: (bs, n_joints)
 
         for idx in range(num_joints):
             heatmap_pred = heatmaps_pred[idx].squeeze() # (bs, hm_h*hm_w)
-            # print('hm_pred: ', heatmap_pred.shape, 'expect: (bs, hm_h*hm_w)')
             heatmap_gt = heatmaps_gt[idx].squeeze() # (bs, hm_h*hm_w)
-            # print('hm_gt: ', heatmap_gt.shape, 'expect: (bs, hm_h*hm_w)')
-            # print('hm_weight: ', target_weight.shape, 'expect: (bs, 196)')
             if self.use_target_weight:
                 loss += 0.5 * self.criterion(
                     heatmap_pred.mul(target_weight[:, idx:idx+1]),
