4b058284d6223bc9d8c3b9a1f47899f1ec9fdd84
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-03-27 22:44:31 +0900

    2_exp0000

--- 0_create_dataset/data0001/check_lmdb.py
+++ 0_create_dataset/data0001/check_lmdb.py
@@ -9,16 +9,23 @@ from pprint import pprint
 def main():
     ROOT_DIR = Path.cwd().parents[2]
     EXP_NAME = Path.cwd().stem
-    LMDB_DIR = ROOT_DIR / 'data' / EXP_NAME / 'lmdb'
+    RAW_LMDB_DIR = ROOT_DIR / 'data' / 'data0001' / 'lmdb'
+    SAVE_LMDB_DIR = ROOT_DIR / 'data' / EXP_NAME / 'lmdb'
 
-    env = lmdb.open(str(LMDB_DIR), max_readers=32, readonly=True, lock=False, readahead=False, meminit=False)
-    
-    with env.begin(write=False) as txn:
+    raw_env = lmdb.open(str(RAW_LMDB_DIR), max_readers=32, readonly=True, lock=False, readahead=False, meminit=False)
+
+
+    with raw_env.begin(write=False) as txn:
         n_samples = int(txn.get('num-samples'.encode()))
-        print(n_samples)
-        for i in [1, n_samples-1]:
-            i += 1
 
+    save_env = lmdb.open(str(SAVE_LMDB_DIR), map_size=int(4e+9))
+
+    count = 0
+
+    for i in range(n_samples):
+        i += 1
+
+        with raw_env.begin(write=False) as txn:
             # image
             img_key = f'image-{str(i).zfill(8)}'.encode()
             imgbuf = txn.get(img_key)
@@ -26,13 +33,15 @@ def main():
             buf.write(imgbuf)
             buf.seek(0)
             img = Image.open(buf).convert('RGB')
-            print(img.size)
-
+            
             # json
             label_key = f'label-{str(i).zfill(8)}'.encode()
             label = txn.get(label_key).decode('utf-8')
             json_dict = json.loads(label)
-            pprint(json_dict)
+
+        for text_dict in json_dict['text']:
+            pass
+            
 
 
 if __name__ == '__main__':
