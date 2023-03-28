9eb6894d062f154463c6350d31ca5d2939a8c470
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-03-28 11:07:06 +0900

    2_exp0000, data0002

--- 0_create_dataset/data0002/main.py
+++ 0_create_dataset/data0002/main.py
@@ -3,44 +3,140 @@ from tqdm import tqdm
 from pathlib import Path
 import lmdb
 import re
-import six
-from PIL import Image
+import cv2
+
+def anns2point_coors(json_dict, img_size):
+    tick_id2text = {text_dict['id']: text_dict['text'] for text_dict in json_dict['text']}
+    
+    axis_dict = json_dict['axes']
+    
+    tick2coor = {}
+    
+    # horizontalの場合は特殊
+    if json_dict['chart-type'] == 'horizontal_bar':
+        x_ticks_temp = axis_dict['x-axis']['ticks']
+        axis_dict['x-axis']['ticks'] = axis_dict['y-axis']['ticks']
+        axis_dict['y-axis']['ticks'] = x_ticks_temp
+    # dotはxがcategoricalっぽいのにnumericalとか言ってる
+    if json_dict['chart-type'] == 'dot':
+        json_dict['axes']['x-axis']['values-type'] = 'categorical'
+
+    def _fix_text(text):
+        text = str(text)
+        text = re.sub('\.$', '', text)
+        text = text.replace(',', '').replace('%', '').replace('..', '.').replace('$', '')
+        return float(text)
+    
+    for ax_key, ax_dict in axis_dict.items():
+        ax_type = ax_key.split('-')[0] # x or y
+        if ax_dict['values-type'] == 'numerical':
+            coors, values = [], []
+            for tick_dict in ax_dict['ticks']:
+                tick_id = tick_dict['id']
+                text = tick_id2text[tick_id]
+                coor = tick_dict['tick_pt'][ax_type]
+                
+                values.append(_fix_text(text))
+                coors.append(coor)
+            a = (coors[-1] - coors[0]) / (values[-1] - values[0])
+            b = coors[-1] - a * values[-1]
+            tick2coor[ax_type] = {'values-type': 'numerical', 'convert': (a, b)}
+            
+        elif ax_dict['values-type'] == 'categorical':
+            text2coor = {}
+            for tick_dict in ax_dict['ticks']:
+                tick_id = tick_dict['id']
+                text = tick_id2text[tick_id]
+                coor = tick_dict['tick_pt'][ax_type]
+                text2coor[text] = coor
+            tick2coor[ax_type] = {'values-type': 'categorical', 'convert': text2coor}
+    
+    ann_coors = []
+    for ann in json_dict['data-series']:
+        ann_coor = {}
+        data = {'x': ann['x'], 'y':ann['y']}
+        for ax_type, ax_tick2coor in tick2coor.items():
+            try:
+                if ax_tick2coor['values-type'] == 'numerical':
+                    a, b = ax_tick2coor['convert']
+                    ann_coor[ax_type] = a * data[ax_type] + b
+                else:
+                    text2coor = ax_tick2coor['convert']
+                    ann_coor[ax_type] = text2coor[data[ax_type]]
+            except:
+                pass
+        # 条件
+        if 'x' not in ann_coor or 'y' not in ann_coor:
+            continue
+        if ann_coor['x'] < 0 or ann_coor['x'] > img_size[1] \
+            or ann_coor['y'] < 0 or ann_coor['y'] > img_size[0]:
+            ann_coors.append(ann_coor)
+    return ann_coors
+
+def add_info_json(json_dict, json_path, img_size):
+    id_ = json_path.stem
+    json_dict['id'] = id_
+    json_dict['key_point'] = anns2point_coors(json_dict, img_size)
+    return json_dict
+
+def write_cache(env, cache):
+    with env.begin(write=True) as txn:
+        for k, v in cache.items():
+            txn.put(k, v)
 
 def main():
     ROOT_DIR = Path.cwd().parents[2]
     EXP_NAME = Path.cwd().stem
-    RAW_LMDB_DIR = ROOT_DIR / 'data' / 'data0001' / 'lmdb'
-    SAVE_LMDB_DIR = ROOT_DIR / 'data' / EXP_NAME / 'lmdb'
+    RAW_DATA_DIR = ROOT_DIR / 'original_data'
+    TRAIN_IMG_DIR = RAW_DATA_DIR / 'train' / 'images'
+    TEST_IMG_DIR = RAW_DATA_DIR / 'train' / 'images'
+    TRAIN_LABEL_DIR = RAW_DATA_DIR / 'train' / 'annotations'
 
-    raw_env = lmdb.open(str(RAW_LMDB_DIR), max_readers=32, readonly=True, lock=False, readahead=False, meminit=False)
+    SAVE_DIR = ROOT_DIR / 'data' / EXP_NAME
+    SAVE_DIR.mkdir(exist_ok=True, parents=True)
+    file_size = 3e9
 
+    img_path_list = list(TRAIN_IMG_DIR.glob('*.jpg'))
+    LMDB_DIR = SAVE_DIR / 'lmdb'
+    LMDB_DIR.mkdir(exist_ok=True, parents=True)
 
-    with raw_env.begin(write=False) as txn:
-        n_samples = int(txn.get('num-samples'.encode()))
+    env = lmdb.open(str(LMDB_DIR), map_size=int(file_size*1.2))
+    cache = {}
+    n_images = len(img_path_list)
 
-    save_env = lmdb.open(str(SAVE_LMDB_DIR), map_size=int(4e+9))
+    count = 1
 
-    count = 0
+    for i, img_path in tqdm(enumerate(img_path_list), total=n_images):
+        id_ = img_path.stem
+        # img
+        with open(img_path, 'rb') as f:
+            img_bin = f.read()
+        _, h, w = cv2.imread(str(img_path)).shape
+        img_size = [h, w]
+        # json
+        json_path = TRAIN_LABEL_DIR / (img_path.stem + '.json')
+        with open(json_path, 'r') as f:
+            json_dict = json.load(f)
+        try:
+            json_dict = add_info_json(json_dict, json_path, img_size)
+        except:
+            continue
+        json_bin = json.dumps(json_dict).encode()
+        # key
+        img_key = f'image-{str(count).zfill(8)}'.encode()
+        label_key = f'label-{str(count).zfill(8)}'.encode()
 
-    for i in range(n_samples):
-        i += 1
+        cache[img_key] = img_bin
+        cache[label_key] = json_bin
 
-        with raw_env.begin(write=False) as txn:
-            # image
-            img_key = f'image-{str(i).zfill(8)}'.encode()
-            imgbuf = txn.get(img_key)
-            buf = six.BytesIO()
-            buf.write(imgbuf)
-            buf.seek(0)
-            img = Image.open(buf).convert('RGB')
-            
-            # json
-            label_key = f'label-{str(i).zfill(8)}'.encode()
-            label = txn.get(label_key).decode('utf-8')
-            json_dict = json.loads(label)
+        if i % 1000 == 0:
+            cache['num-samples'.encode()] = str(count).encode()
+            write_cache(env, cache)
+            cache = {}
+        
+        count += 1
+    cache['num-samples'.encode()] = str(count).encode()
+    write_cache(env, cache)
 
-        for text_dict in json_dict['text']:
-            pass
-    
 if __name__ == '__main__':
     main()
---
+++ 0_create_dataset/data0003/main.py
@@ -0,0 +1,46 @@
+import json
+from tqdm import tqdm
+from pathlib import Path
+import lmdb
+import re
+import six
+from PIL import Image
+
+def main():
+    ROOT_DIR = Path.cwd().parents[2]
+    EXP_NAME = Path.cwd().stem
+    RAW_LMDB_DIR = ROOT_DIR / 'data' / 'data0001' / 'lmdb'
+    SAVE_LMDB_DIR = ROOT_DIR / 'data' / EXP_NAME / 'lmdb'
+
+    raw_env = lmdb.open(str(RAW_LMDB_DIR), max_readers=32, readonly=True, lock=False, readahead=False, meminit=False)
+
+
+    with raw_env.begin(write=False) as txn:
+        n_samples = int(txn.get('num-samples'.encode()))
+
+    save_env = lmdb.open(str(SAVE_LMDB_DIR), map_size=int(4e+9))
+
+    count = 0
+
+    for i in range(n_samples):
+        i += 1
+
+        with raw_env.begin(write=False) as txn:
+            # image
+            img_key = f'image-{str(i).zfill(8)}'.encode()
+            imgbuf = txn.get(img_key)
+            buf = six.BytesIO()
+            buf.write(imgbuf)
+            buf.seek(0)
+            img = Image.open(buf).convert('RGB')
+            
+            # json
+            label_key = f'label-{str(i).zfill(8)}'.encode()
+            label = txn.get(label_key).decode('utf-8')
+            json_dict = json.loads(label)
+
+        for text_dict in json_dict['text']:
+            pass
+    
+if __name__ == '__main__':
+    main()
