38788e68fbeba4aaa40c181804a289f86e859c18
Luka-Magic <74438554+Luka-Magic@users.noreply.github.com>
2023-06-11 18:37:56 +0900

    2_3

--- 2_keypoints_detection/exp0003/main.py
+++ 2_keypoints_detection/exp0003/main.py
@@ -312,6 +312,9 @@ def train_one_epoch(cfg, epoch, dataloader, model, loss_fn, device, optimizer, s
         pred_n_points = np.sum(torch.sigmoid(
             pred).detach().cpu().numpy() > 0.5, axis=(1, 2))
         gt_n_points = n_points.numpy()
+        if step < 2:
+            print(pred_n_points)
+            print(gt_n_points)
         acc = np.mean(pred_n_points == gt_n_points)
 
         accuracy.update(acc, bs)
